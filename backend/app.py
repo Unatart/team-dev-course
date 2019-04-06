eabccdf321c7fb5d2648e6e1a1500d7c2f8b570a
MikeGus <vfc17@yandex.ru>
2019-04-06 19:04:24 +0300

    Fix #13. Backend refactor. --errors-only flag to pylint

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,13 @@
-from routes.database import *
-from appconfig import APP
+from flask import Flask
+from flask_cors import CORS
+
+from routes import ECONOMICS_APP
+
+
+APP = Flask(__name__)
+APP.register_blueprint(ECONOMICS_APP)
+CORS(APP, supports_credentials=True)
+
 
 if __name__ == '__main__':
     APP.run(threaded=True, debug=False)
--- backend/routes.py
+++ backend/routes.py
@@ -1,22 +1,22 @@
 import json
 import peewee
-from flask import request, abort, make_response, jsonify
+from flask import request, abort, make_response, jsonify, Blueprint
 
-from models.database import User
-from repository.database import Repository
-from appconfig import APP, DB
+from database_models import User
+from database_wrapper import Repository
+from appconfig import DB
 
 
 REPOSITORY = Repository(DB)
+ECONOMICS_APP = Blueprint('app', __name__)
 
 
-@APP.route('/api/users', methods=['POST'])
+@ECONOMICS_APP.route('/api/users', methods=['POST'])
 def create_user():
     try:
         req_user = json.loads(request.data)
         resp_user = REPOSITORY.create_user(req_user)
-        resp = make_response(jsonify(resp_user), 201)
-        return resp
+        return make_response(jsonify(resp_user), 201)
     except peewee.IntegrityError:
         abort(400)
     except ValueError:
@@ -25,14 +25,13 @@ def create_user():
         abort(400)
 
 
-@APP.route('/api/login', methods=['POST'])
+@ECONOMICS_APP.route('/api/login', methods=['POST'])
 def login():
     try:
         req_user = json.loads(request.data)
         resp_user, status_code = REPOSITORY.login(req_user)
         if status_code == 403:
             abort(403)
-
         return make_response(jsonify(resp_user))
     except KeyError:
         abort(400)
