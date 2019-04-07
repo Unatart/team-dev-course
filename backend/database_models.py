4b9572334a6e14c8b8ae45b8907c4c363b5aa7a6
MikeGus <vfc17@yandex.ru>
2019-04-07 18:52:15 +0300

    Fix #9, Fix #10. Added spendings/arrivals/etc. Minor fixes

--- backend/database_models.py
+++ backend/database_models.py
@@ -1,13 +1,13 @@
-from peewee import Model, CharField
+from peewee import Model, CharField, IntegerField, TextField, DateField, ForeignKeyField
 from appconfig import DB
 
 
 def init_tables():
-    DB.create_tables([User], safe=True)
+    DB.create_tables([User, Arrival, Category, Spending], safe=True)
 
 
 def drop_tables():
-    DB.drop_tables([User])
+    DB.drop_tables([User, Arrival, Category, Spending])
 
 
 class BaseModel(Model):
@@ -19,3 +19,23 @@ class User(BaseModel):
     username = CharField(unique=True)
     email = CharField(unique=True)
     password = CharField()
+
+
+class Arrival(BaseModel):
+    amount = IntegerField()
+    description = TextField()
+    date = DateField()
+    user = ForeignKeyField(model=User, on_delete='CASCADE')
+
+
+class Category(BaseModel):
+    description = TextField()
+    user = ForeignKeyField(model=User, on_delete='CASCADE')
+
+
+class Spending(BaseModel):
+    amount = IntegerField()
+    description = TextField()
+    date = DateField()
+    category = ForeignKeyField(model=Category, on_delete='CASCADE')
+    user = ForeignKeyField(model=User, on_delete='CASCADE')
--- backend/routes.py
+++ backend/routes.py
@@ -32,8 +32,95 @@ def login():
         resp_user, status_code = REPOSITORY.login(req_user)
         if status_code == 403:
             abort(403)
-        return make_response(jsonify(resp_user))
+        return make_response(jsonify(resp_user), 200)
     except KeyError:
         abort(400)
     except User.DoesNotExist:
         abort(404)
+
+
+@ECONOMICS_APP.route('/api/arrivals', methods=['POST'])
+def add_arrival():
+    req = json.loads(request.data)
+    resp, _ = REPOSITORY.add_arrival(req)
+    return make_response(jsonify(resp), 201)
+
+
+@ECONOMICS_APP.route('/api/arrivals/<arrival_id>', methods=['POST'])
+def update_arrival(arrival_id):
+    try:
+        req = json.loads(request.data)
+        resp, status_code = REPOSITORY.update_arrival(req, arrival_id)
+        if status_code == 403:
+            abort(403)
+        return make_response(jsonify(resp), 200)
+    except KeyError:
+        abort(400)
+
+
+@ECONOMICS_APP.route('/api/arrivals/<arrival_id>', methods=['DELETE'])
+def delete_arrival(arrival_id):
+    try:
+        req = json.loads(request.data)
+        resp, status_code = REPOSITORY.delete_arrival(req, arrival_id)
+        if status_code == 403:
+            abort(403)
+        return make_response(jsonify(resp), 200)
+
+    except KeyError:
+        abort(400)
+
+
+@ECONOMICS_APP.route('/api/categories', methods=['POST'])
+def add_category():
+    try:
+        req = json.loads(request.data)
+        resp, _ = REPOSITORY.add_category(req)
+        return make_response(jsonify(resp), 201)
+    except KeyError:
+        abort(400)
+
+
+@ECONOMICS_APP.route('/api/spendings', methods=['POST'])
+def add_spending():
+    try:
+        req = json.loads(request.data)
+        resp, _ = REPOSITORY.add_spending(req)
+        return make_response(jsonify(resp), 201)
+    except KeyError:
+        abort(400)
+
+
+@ECONOMICS_APP.route('/api/spendings/<spending_id>', methods=['POST'])
+def update_spending(spending_id):
+    try:
+        req = json.loads(request.data)
+        resp, status_code = REPOSITORY.update_spending(req, spending_id)
+        if status_code == 403:
+            abort(403)
+        return make_response(jsonify(resp), 200)
+    except KeyError:
+        abort(400)
+
+
+@ECONOMICS_APP.route('/api/spendings/<spending_id>', methods=['DELETE'])
+def delete_spending(spending_id):
+    try:
+        req = json.loads(request.data)
+        resp, status_code = REPOSITORY.delete_spending(req, spending_id)
+        if status_code == 403:
+            abort(403)
+        return make_response(jsonify(resp), 200)
+    except KeyError:
+        abort(400)
+
+
+@ECONOMICS_APP.route('/api/users/<string:username>', methods=['GET'])
+def get_user_tables(username):
+    try:
+        resp, status_code = REPOSITORY.get_user_tables(username)
+        if status_code == 403:
+            abort(403)
+        return make_response(jsonify(resp), 200)
+    except KeyError:
+        abort(400)
