e51da0f8689037291b9abc00d415c2770958abc0
Unatart <unatart@outlook.com>
2019-04-07 14:58:41 +0300

    Fix#4 and Fix#5. Add categories, spendings table.

--- front/dist/static/js/spendingsTable.js
+++ front/dist/static/js/spendingsTable.js
@@ -4,7 +4,19 @@ addRowButton2.addEventListener('click', () => {
     const descr = document.getElementById('description2').value;
     const money = document.getElementById('money2').value;
     const category = document.getElementById('category');
-    const selected = category.options[category.selectedIndex].value;// ??? активный элемент
+    const selected = category.options[category.selectedIndex].value;
+
+    const currentChild = {'description': descr, 'money': money, 'category': selected};
+    let spendingsList = window.localStorage.getItem('spendingsList');
+    if (spendingsList == null) {
+        let spdList = [];
+        spdList.push(currentChild);
+        window.localStorage.setItem('spendingsList', JSON.stringify(spdList));
+    } else {
+        let spdList = JSON.parse(spendingsList);
+        spdList.push(currentChild);
+        window.localStorage.setItem('spendingsList', JSON.stringify(spdList));
+    }
 
     if (descr !== '' && money !== '') {
 
@@ -35,21 +47,33 @@ function editRowS(n) {
 
     let descr = document.getElementById("descrRowS"+n);
     let money = document.getElementById("moneyRowS"+n);
+    let category = document.getElementById("categoryRowS"+n);
 
     let descrData = descr.innerHTML;
     let moneyData = money.innerHTML;
 
     descr.innerHTML = "<input type = 'text' id = 'descrTextS" + n + "' value = '" + descrData + "'>";
     money.innerHTML = "<input type = 'text' id = 'moneyTextS" + n + "' value = '" + moneyData + "'>";
+    category.innerHTML = "<select id = 'newCategory'></select>";
+
+    let arrCategory = JSON.parse(window.localStorage.getItem('category'));
+    let select = document.getElementById('newCategory');
+    for (let i = 0, length = arrCategory.length; i < length; i++) {
+        select.options[i] = new Option(arrCategory[i]);
+    }
 }
 
 
 function saveRowS(n) {
     let descr = document.getElementById("descrTextS"+n).value;
     let money = document.getElementById("moneyTextS"+n).value;
+    let category = document.getElementById("newCategory");
+    const selected = category.options[category.selectedIndex].value;
+
 
     document.getElementById("descrRowS"+n).innerHTML = descr;
     document.getElementById("moneyRowS"+n).innerHTML = money;
+    document.getElementById("categoryRowS"+n).innerHTML = selected;
 
     document.getElementById("editButtonS"+n).style.display = "block";
     document.getElementById("saveButtonS"+n).style.display = "none";
