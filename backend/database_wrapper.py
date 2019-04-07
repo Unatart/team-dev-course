2c98da07e3782b4f063b3dbd4f47771e61111135
MikeGus <vfc17@yandex.ru>
2019-04-07 18:52:15 +0300

    Fix #9, Fix #10. Added spendings/arrivals/etc. Minor fixes

--- backend/database_wrapper.py
+++ backend/database_wrapper.py
@@ -1,5 +1,5 @@
 from passlib.hash import pbkdf2_sha256
-from database_models import User
+from database_models import User, Arrival, Category, Spending
 from validator import Validator
 
 
@@ -36,3 +36,147 @@ class Repository:
             if not pbkdf2_sha256.verify(user['password'], db_user.password):
                 return {}, 403
             return {'username': db_user.username, 'email': db_user.email}, 200
+
+    def add_arrival(self, req):
+        with self.database.atomic():
+            db_user = User.get(User.username == req['username'])
+
+            amount = req['amount']
+            description = req['description']
+            date = req['date']
+
+            new_arrival = Arrival(amount=amount, description=description, date=date, user=db_user)
+            new_arrival.save()
+
+            return {'id': new_arrival.get_id()}, 201
+
+    def update_arrival(self, req, arrival_id):
+        with self.database.atomic():
+            arrival = Arrival.get_by_id(arrival_id)
+            if arrival is not None:
+                user = User.get(User.username == req['username'])
+                if arrival.user == user:
+                    arrival.amount = req['amount']
+                    arrival.description = req['description']
+                    arrival.date = req['date']
+                    arrival.save()
+                else:
+                    return {}, 403
+            else:
+                return {}, 404
+            return {
+                'id': arrival.get_id(), 'amount': arrival.amount, 'description': arrival.description,
+                'date': arrival.date
+            }, 200
+
+    def delete_arrival(self, req, arrival_id):
+        with self.database.atomic():
+            user = User.get(User.username == req['username'])
+            arrival = Arrival.get_by_id(arrival_id)
+            if arrival is not None:
+                if arrival.user == user:
+                    arrival.delete_instance()
+                else:
+                    return {}, 403
+            else:
+                return {}, 404
+            return {}, 200
+
+    def add_category(self, req):
+        with self.database.atomic():
+            user = User.get(User.username == req['username'])
+            description = req['description']
+
+            category = Category(description=description, user=user)
+            category.save()
+
+            return {'id': category.get_id()}, 201
+
+    def add_spending(self, req):
+        with self.database.atomic():
+            user = User.get(User.username == req['username'])
+
+            amount = req['amount']
+            description = req['description']
+            date = req['date']
+            category = Category.get(Category.id == req['category_id'] and Category.user == user)
+
+            spending = Spending(amount=amount, description=description, date=date, category=category, user=user)
+            spending.save()
+
+            return {'id': spending.get_id()}, 201
+
+    def update_spending(self, req, spending_id):
+        with self.database.atomic():
+            user = User.get(User.username == req['username'])
+
+            spending = Spending.get_by_id(spending_id)
+            if spending is not None:
+                if spending.user == user:
+                    spending.amount = req['amount']
+                    spending.description = req['description']
+                    spending.date = req['date']
+                    spending.category = Category.get(Category.id == req['category_id'] and Category.user == user)
+                    spending.save()
+                else:
+                    return {}, 403
+            else:
+                return {}, 404
+            return {
+                'id': spending.get_id(), 'amount': spending.amount, 'description': spending.description,
+                'date': spending.date, 'category': spending.category.description
+            }, 200
+
+    def delete_spending(self, req, spending_id):
+        with self.database.atomic():
+            user = User.get(User.username == req['username'])
+            spending = Spending.get_by_id(spending_id)
+            if spending is not None:
+                if spending.user == user:
+                    spending.delete_instance()
+                else:
+                    return {}, 403
+            else:
+                return {}, 404
+            return {}, 200
+
+    def get_user_tables(self, username):
+        with self.database.atomic():
+            user = User.get(User.username == username)
+
+            arrivals = [
+                {
+                    "amount": arrival.amount,
+                    "date": str(arrival.date),
+                    "id": arrival.id
+                }
+                for arrival in Arrival.select(
+                    Arrival.amount, Arrival.description, Arrival.date, Arrival.id
+                ).where(Arrival.user == user)
+            ]
+
+            categories = [
+                {
+                    "description": category.description,
+                    "id": category.id
+                }
+                for category in Category.select(
+                    Category.description, Category.id
+                ).where(Category.user == user)
+            ]
+
+            spendings = [
+                {
+                    "amount": spending.amount,
+                    "date": str(spending.date),
+                    "category": spending.category.description,
+                    "id": spending.id
+                }
+                for spending in Spending.select(
+                    Spending.amount, Spending.description, Spending.date, Spending.category, Spending.id
+                ).where(Spending.user == user)
+            ]
+
+            return {
+                'arrivals': arrivals, 'categories': categories, 'spendings': spendings, 'user': user.username
+            }, 200
