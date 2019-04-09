c3c805ec26a556607323ad0b6e0bbc99e7574030
Unatart <unatart@outlook.com>
2019-04-09 15:32:28 +0300

    Fix#17 and Fix#19 Application works well, add doughnut chart draw

---
+++ frontend/static/board/js/chart/chart.js
@@ -0,0 +1,89 @@
+'use strict';
+
+import {getCookie} from "../../../auth/js/cookie.js";
+import {checkStatus} from "../info/utils.js";
+
+
+const dateButton = document.getElementById('dateButton');
+
+dateButton.addEventListener('click', () => {
+    const startDate = formatDate(document.getElementById('startDate').value);
+    const finishDate = formatDate(document.getElementById('finishDate').value);
+    console.log(startDate, finishDate);
+    let opts = {'start_date': startDate, 'finish_date': finishDate, 'username': getCookie('username')};
+    getChartInfo(opts);
+});
+
+function formatDate(input) {
+    return input.split('-').reverse().join('-');
+}
+
+function getChartInfo(opts) {
+    function receive() {
+        let init = {
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'include',
+            headers: {
+                "Access-Control-Allow-Credentials": "true",
+                'Access-Control-Allow-Methods': 'POST',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(opts)
+        };
+
+        fetch(('http://127.0.0.1:5000/api/chart'), init)
+            .then(checkStatus)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                createChart(data);
+            })
+            .catch(error => console.error(error));
+    }
+
+    return receive();
+}
+
+function createChart(data) {
+    let arrData = [];
+    let labelsData = [];
+    for (let elem in data) {
+        for (let i in data[elem]) {
+            let ind = data[elem][i];
+            arrData.push(ind.money*100);
+            labelsData.push(ind.category);
+        }
+    }
+    let rgbArr = randomColor({
+        count: arrData.length,
+        hue: 'red',
+        luminosity: 'bright'
+    });
+
+    let ctx = document.getElementById('chartPie').getContext('2d');
+    ctx.canvas.width = 300;
+    ctx.canvas.height = 300;
+
+    let pieChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: labelsData,
+            datasets: [{
+                data: arrData,
+                backgroundColor: rgbArr,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            legend: {
+                labels: {
+                    fontColor: 'white'
+                }
+            }
+        }
+
+    });
+}
+
---
+++ frontend/static/board/js/table/spendingsTable.js
@@ -0,0 +1,118 @@
+'use strict';
+
+import {deleteRowS, editRowS, saveRowS} from "./optSpd.js";
+import {getArrTotal, getSpdTotal, style} from "./utils.js";
+import {getTime} from "./utils.js";
+import {getCookie} from "../../../auth/js/cookie.js";
+import {checkStatus, parseJSON} from "../info/utils.js";
+
+let addRowButtonS = document.getElementById('addRow2');
+
+addRowButtonS.addEventListener('click', () => {
+    const descr = document.getElementById('description2').value;
+    const money = document.getElementById('money2').value;
+    const category = document.getElementById('category');
+    const selected = category.options[category.selectedIndex].value;
+    console.log(selected);
+
+    if (descr !== '' && money !== '') {
+        if (+money > 0) {
+            addToRemoteS('http://127.0.0.1:5000/api/spendings', descr, money, selected);
+        }
+
+        document.getElementById('description2').value = '';
+        document.getElementById('money2').value = '';
+
+    }
+});
+
+function addToLocalS(descr, money, selected, id) {
+    let d = getTime();
+    const currentChild = {'id': id, 'description': descr, 'money': money, 'category': selected, 'data': d};
+    let spendingsList = window.localStorage.getItem('spendingsList');
+
+    if (spendingsList === 'null' || spendingsList == null) {
+        if (getArrTotal() - getSpdTotal() - currentChild['money'] >= 0) {
+            let spdList = [];
+            spdList.push(currentChild);
+            window.localStorage.setItem('spendingsList', JSON.stringify(spdList));
+            window.localStorage.setItem('balance', +window.localStorage.getItem('balance') - currentChild['money']);
+        }
+        else {
+            alert('addToLocalS negative total');
+        }
+
+    } else {
+        if (getArrTotal() - getSpdTotal() - currentChild['money'] >= 0) {
+            let spdList = JSON.parse(spendingsList);
+            spdList.push(currentChild);
+            window.localStorage.setItem('spendingsList', JSON.stringify(spdList));
+            window.localStorage.setItem('balance', +window.localStorage.getItem('balance') - currentChild['money']);
+        }
+        else {
+            alert('addToLocalS negative total');
+        }
+    }
+
+    createRowS(descr, money, selected, d);
+}
+
+function addToRemoteS(address, descr, money, category) {
+    let today = getTime();
+    console.log(category);
+    let opt = {'username': getCookie('username'), 'description': descr, 'amount': money, 'date': today, 'category': category};
+
+    function sendInfo() {
+        let init = {
+            method: 'POST',
+            mode: 'cors',
+            credentials: 'include',
+            headers: {
+                "Access-Control-Allow-Credentials": "true",
+                'Access-Control-Allow-Methods': 'POST',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(opt)
+        };
+
+        fetch(address, init)
+            .then(checkStatus)
+            .then(parseJSON)
+            .then(data => {
+                let id = data['id'];
+                addToLocalS(descr, money, category, id);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    sendInfo();
+}
+
+function createRowS(descr, money, selected, date) {
+    document.getElementById('balance').innerHTML = window.localStorage.getItem('balance');
+
+    console.log(date);
+    let table = document.getElementById('spendingsTable');
+    let tableLength = table.rows.length;
+    table.insertRow(tableLength).outerHTML =
+        "<tr id='rowS" + tableLength + "'>" +
+            "<td id='descrRowS" + tableLength + "'>" + descr + "</td>" +
+            "<td id='moneyRowS" + tableLength + "'>" + money + "</td>" +
+            "<td id='categoryRowS" + tableLength + "'>" + selected + "</td>" +
+            "<td id='dateRowS'>" + date + "</td>" +
+            "<td>" +
+                "<input style='display: block; " + style + "' type='button' id='editButtonS" + tableLength + "' value='Edit' class='edit' onclick='editRowS(" + tableLength + ")'>" +
+                "<input style='display: none; " + style + "' type='button' id='saveButtonS" + tableLength + "' value='Save' class='save' onclick='saveRowS(" + tableLength + ")'> " +
+                "<input style='display: block; " + style + "' type='button' id='deleteButtonS" + tableLength + "' value='Delete' class='delete' onclick='deleteRowS(" + tableLength + ")' > " +
+            "</td>" +
+        "</tr>";
+
+    document.getElementById('editButtonS' + tableLength).onclick = editRowS.bind({...this, tableLength});
+    document.getElementById('saveButtonS' + tableLength).onclick = saveRowS.bind({...this, tableLength});
+    document.getElementById('deleteButtonS' + tableLength).onclick = deleteRowS.bind({...this, tableLength});
+}
+
+
