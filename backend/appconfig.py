8706ce7fd499e65d281b57c39c86f9e21cee0d5e
MikeGus <vfc17@yandex.ru>
2019-04-14 21:06:22 +0300

    Added chart data and backend testing. Fix #21

--- backend/appconfig.py
+++ backend/appconfig.py
@@ -2,10 +2,9 @@ import os
 
 from peewee import PostgresqlDatabase
 
-
 DB = PostgresqlDatabase(
-    os.environ.get('ECONAPP_DATABASE'),
-    user=os.environ.get('ECONAPP_DATABASE_USER'),
-    password=os.environ.get('ECONAPP_DATABASE_PASSWORD'),
-    host=os.environ.get('ECONAPP_DATABASE_HOST', 'localhost')
+     os.environ.get('ECONAPP_DATABASE'),
+     user=os.environ.get('ECONAPP_DATABASE_USER'),
+     password=os.environ.get('ECONAPP_DATABASE_PASSWORD'),
+     host=os.environ.get('ECONAPP_DATABASE_HOST', 'localhost')
 )
