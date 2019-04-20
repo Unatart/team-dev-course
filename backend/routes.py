71260f036d321dd338c94f16917f490eb08ad426
Unatart <unatart@outlook.com>
2019-04-20 22:33:31 +0300

    Add routes docu

--- backend/routes.py
+++ backend/routes.py
@@ -1,3 +1,9 @@
+""" @package routes
+Application routes
+Arguments:
+    REPOSITORY (Repository): db wrapper
+    ECONOMICS_APP (flask.Blueprint): app blueprint
+"""
 import json
 import peewee
 from flask import request, abort, make_response, jsonify, Blueprint
@@ -13,6 +19,14 @@ ECONOMICS_APP = Blueprint('app', __name__)
 
 @ECONOMICS_APP.route('/api/users', methods=['POST'])
 def create_user():
+    """ Get request data and call wrapper func for user creation
+    Args:
+        request
+    Returns:
+        response, 201 if wrapper returns 201
+
+        400 otherwise
+    """
     try:
         req_user = json.loads(request.data)
         resp_user = REPOSITORY.create_user(req_user)
@@ -27,6 +41,18 @@ def create_user():
 
 @ECONOMICS_APP.route('/api/login', methods=['POST'])
 def login():
+    """ Get request data and call wrapper func for login
+    Args:
+        request
+    Returns:
+        response, 200 if wrapper returns 200
+
+        403 if wrapper returns 403
+
+        400 if KeyError
+
+        404 if peewee: User.DoesNotExist
+    """
     try:
         req_user = json.loads(request.data)
         resp_user, status_code = REPOSITORY.login(req_user)
@@ -41,6 +67,12 @@ def login():
 
 @ECONOMICS_APP.route('/api/arrivals', methods=['POST'])
 def add_arrival():
+    """ Get request and call wrapper func for arrival creation
+    Args:
+        request
+    Returns:
+        response, 201
+    """
     req = json.loads(request.data)
     resp, _ = REPOSITORY.add_arrival(req)
     return make_response(jsonify(resp), 201)
@@ -48,6 +80,16 @@ def add_arrival():
 
 @ECONOMICS_APP.route('/api/arrivals/<arrival_id>', methods=['POST'])
 def update_arrival(arrival_id):
+    """ Get request data and arrival id, call wrapper func for update arrival
+    Args:
+        request
+    Returns:
+        response, 200 if wrapper returns 200
+
+        403 if wrapper returns 403 (user doesn't have permission)
+
+        400 if KeyError
+    """
     try:
         req = json.loads(request.data)
         resp, status_code = REPOSITORY.update_arrival(req, arrival_id)
@@ -60,6 +102,16 @@ def update_arrival(arrival_id):
 
 @ECONOMICS_APP.route('/api/arrivals/<arrival_id>', methods=['DELETE'])
 def delete_arrival(arrival_id):
+    """ Get request data and arrival id, call wrapper func for delete arrival
+    Args:
+        request
+    Returns:
+        response, 200 if wrapper returns 200
+
+        403 if wrapper returns 403 (user doesn't have permission)
+
+        400 if KeyError
+    """
     try:
         req = json.loads(request.data)
         resp, status_code = REPOSITORY.delete_arrival(req, arrival_id)
@@ -73,6 +125,14 @@ def delete_arrival(arrival_id):
 
 @ECONOMICS_APP.route('/api/categories', methods=['POST'])
 def add_category():
+    """ Get request data and call wrapper func for category creation
+    Args:
+        request
+    Returns:
+        response, 201 if wrapper returns 201
+
+        400 if KeyError
+    """
     try:
         req = json.loads(request.data)
         resp, _ = REPOSITORY.add_category(req)
@@ -83,6 +143,14 @@ def add_category():
 
 @ECONOMICS_APP.route('/api/spendings', methods=['POST'])
 def add_spending():
+    """ Get request data and call wrapper func for spending creation
+    Args:
+        request
+    Returns:
+        response, 201 if wrapper returns 201
+
+        400 if KeyError
+    """
     try:
         req = json.loads(request.data)
         resp, _ = REPOSITORY.add_spending(req)
@@ -93,6 +161,16 @@ def add_spending():
 
 @ECONOMICS_APP.route('/api/spendings/<spending_id>', methods=['POST'])
 def update_spending(spending_id):
+    """ Get request data, spending id and call wrapper func for update spending
+    Args:
+        request
+    Returns:
+        response, 200 if wrapper returns 200
+
+        403 if wrapper returns 403 (user doesn't have permission)
+
+        400 if KeyError
+    """
     try:
         req = json.loads(request.data)
         resp, status_code = REPOSITORY.update_spending(req, spending_id)
@@ -105,6 +183,16 @@ def update_spending(spending_id):
 
 @ECONOMICS_APP.route('/api/spendings/<spending_id>', methods=['DELETE'])
 def delete_spending(spending_id):
+    """ Get request data, spending id and call wrapper func for delete spending
+    Args:
+        request
+    Returns:
+        response, 200 if wrapper returns 200
+
+        403 if wrapper returns 403 (user doesn't have permission)
+
+        400 if KeyError
+    """
     try:
         req = json.loads(request.data)
         resp, status_code = REPOSITORY.delete_spending(req, spending_id)
@@ -117,6 +205,16 @@ def delete_spending(spending_id):
 
 @ECONOMICS_APP.route('/api/users/<username>', methods=['GET'])
 def get_user_tables(username):
+    """ Get username and call wrapper func for get user info
+    Args:
+        request
+    Returns:
+        response, 200 if wrapper returns 200
+
+        403 if wrapper returns 403 (user doesn't have permission)
+
+        400 if KeyError
+    """
     try:
         resp, status_code = REPOSITORY.get_user_tables(username)
         if status_code == 403:
@@ -128,6 +226,16 @@ def get_user_tables(username):
 
 @ECONOMICS_APP.route('/api/chart', methods=['POST'])
 def get_chart_info():
+    """ Get request data and call wrapper func for get chart info
+    Args:
+        request
+    Returns:
+        response, 200 if wrapper returns 200
+
+        403 if wrapper returns 403 (user doesn't have permission)
+
+        400 if KeyError
+    """
     try:
         req = json.loads(request.data)
         resp, status_code = REPOSITORY.get_chart_info(req)
